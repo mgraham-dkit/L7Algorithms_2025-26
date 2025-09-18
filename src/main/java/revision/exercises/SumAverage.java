bda147a1f64d384ccb3a7f9b0ab0bd39550a9963
michelle <michelle.graham@dkit.ie>
2025-09-18 12:07:15 +0100

    feat: Added program to calculate sum and average

---
+++ src/main/java/revision/exercises/SumAverage.java
@@ -0,0 +1,23 @@
+package revision.exercises;
+
+import java.util.Scanner;
+
+public class SumAverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num = 0;
+        int sum = 0;
+        int count = 0;
+        while(num != -1){
+            System.out.println("Please enter a number (-1 to end):");
+            num = input.nextInt();
+            // sum = sum + num;
+            sum += num;
+        }
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + (sum/count));
+    }
+}
+
